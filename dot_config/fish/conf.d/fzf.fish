1b1f037a26060b1f37791cd1c4c0fad1123758f4
AristotleH <53080395+AristotleH@users.noreply.github.com>
2025-02-17 00:14:22 -0800

    change fish theme, update zsh config

---
+++ dot_config/fish/conf.d/fzf.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+
+# Set up fzf key bindings
+fzf --fish | source
---
+++ dot_config/fish/conf.d/hydro_config.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+set -g hydro_color_pwd blue
+set -g hydro_color_git green
+set -g hydro_color_duration yellow
