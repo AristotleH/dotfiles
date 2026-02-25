e5b3171a3c52ce6a52a78157357af8fe981cfe14
AristotleH <53080395+AristotleH@users.noreply.github.com>
2026-02-25 00:22:15 -0800

    tide truncation

--- dot_config/fish/conf.d/0_env.fish
+++ dot_config/fish/conf.d/0_env.fish
@@ -3,3 +3,8 @@
 
 # Disable greeting message (use -g not -U to avoid setting universal var repeatedly)
 set -g fish_greeting
+
+# Tide prompt truncation settings (configured via official tide variables).
+set -g tide_git_truncation_strategy l
+set -g tide_git_truncation_length 24
+set -g tide_prompt_min_cols 70
